1c59bede2c7c8924d79ab98e75ec7f39ac2905fd
Thomas Steinert <t.steinert@conversis.de>
2018-02-27 14:26:27 +0100

    Fix if-equatation from == to -eq

--- virtualbox.sh
+++ virtualbox.sh
@@ -3,7 +3,7 @@
 OS_CODENAME=$(cat /etc/*-release | grep -i codename | head -n1 | cut -d"=" -f2)
 PKG=virtualbox
 PKG_URL=http://download.virtualbox.org/virtualbox
-PKG_LATEST=$(curl ${PKG_URL} 2>/dev/null | grep -oE "[0-9]+\.[0-9]+\.[0-9]+" | sort -n | tail -n1)
+PKG_LATEST=$(curl ${PKG_URL} 2>/dev/null | grep -oE "[0-9]+\.[0-9]+\.[0-9]+" | tail -n1)
 PKG_FILE=$(curl -sSL ${PKG_URL}/${PKG_LATEST} | sed -e 's/<[^>]*>//g' | grep -oiE "${PKG}.*${PKG_LATEST}.*${OS_CODENAME}_amd64.deb")
 PKG_INSTALLED=$(vboxmanage -v 2>/dev/null)
 
@@ -26,7 +26,7 @@ then
     fi
     sudo dpkg -i ${PKG_FILE}
     sudo apt-get install -f -y
-    if [ $? == 0]
+    if [ $? -eq 0 ]
     then
         sudo usermod -aG vboxusers $(whoami)
     fi
