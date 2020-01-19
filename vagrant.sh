28ccc67dcf4b652ee4728afa1b23e03073c8cf9a
moenka <moenka@10forge.org>
2020-01-19 11:28:55 +0100

    Add test for necessary installation tools
    to prevent the script setting vagrant into an unusuable state which
    needs to be fixed manually.

--- vagrant.sh
+++ vagrant.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+set -e
+
 PKG=vagrant
 PKG_URL=https://releases.hashicorp.com/vagrant
 PKG_VERSION=$(curl -sSL ${PKG_URL}/ | grep -oE "${PKG}_[0-9\.]*" | head -n 1 | cut -d"_" -f2)
 PKG_FILE=vagrant_${PKG_VERSION}_x86_64.deb
 PKG_INSTALLED=$(dpkg -l vagrant 2>/dev/null | grep vagrant | awk '{print $3}' | cut -d":" -f2)
+PKG_TOOLS=(python-pip)
+
+for tool in "${PKG_TOOLS[@]}"; do
+    dpkg -s $tool | grep --color=Never -E "^(Package|Status)"
+done
 
 if [[ ! "${PKG_INSTALLED}" == "${PKG_VERSION}" ]]
 then
